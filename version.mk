df73c6e77d9fe549f1ba7eaddb92d63c2b709d10
Harsh Tagra <74857033+Harsh-Tagra@users.noreply.github.com>
2024-06-05 10:04:03 +0530

    Add files via upload

---
+++ version.mk
@@ -0,0 +1,72 @@
+# (C) 2023-2024 RisingOS
+
+# RisingOS versioning
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/rising/version
+
+RISING_FLAVOR := UDC
+RISING_VERSION := 3.2
+RISING_CODENAME := Janus
+RISING_RELEASE_TYPE := RELEASE
+RISING_CODE := $(RISING_VERSION)
+
+RISING_BUILD_DATE := $(shell date -u +%Y%m%d)
+
+CURRENT_DEVICE := $(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
+MAINTAINER_LIST := $(shell cat vendor/risingOTA/risingOS.maintainers)
+DEVICE_LIST := $(shell cat vendor/risingOTA/risingOS.devices)
+
+ifeq ($(filter $(CURRENT_DEVICE),$(DEVICE_LIST)), $(CURRENT_DEVICE))
+    ifdef RISING_MAINTAINER
+        ifneq ($(filter $(RISING_MAINTAINER),$(MAINTAINER_LIST)),)
+            RISING_BUILDTYPE := OFFICIAL
+        else
+            RISING_BUILDTYPE := UNOFFICIAL
+        endif
+    else
+        RISING_BUILDTYPE := UNOFFICIAL
+    endif
+else
+    RISING_BUILDTYPE := COMMUNITY
+endif
+
+ifeq ($(WITH_GMS), true)
+	ifeq ($(TARGET_CORE_GMS), true)
+    	RISING_PACKAGE_TYPE ?= CORE
+	else 
+    	RISING_PACKAGE_TYPE ?= GAPPS
+	endif
+else
+    RISING_PACKAGE_TYPE ?= VANILLA
+endif
+
+# Build version
+RISING_BUILD_VERSION := $(RISING_VERSION)-$(RISING_RELEASE_TYPE)-$(RISING_BUILD_DATE)-$(RISING_PACKAGE_TYPE)-$(RISING_BUILDTYPE)-$(CURRENT_DEVICE)
+
+# Display version
+RISING_DISPLAY_VERSION := $(RISING_VERSION)-$(RISING_RELEASE_TYPE)-$(RISING_PACKAGE_TYPE)-$(RISING_BUILDTYPE)-$(CURRENT_DEVICE)
+
+# RisingOS properties
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.rising.maintainer=$(RISING_MAINTAINER) \
+    ro.rising.code=$(RISING_CODENAME) \
+    ro.rising.packagetype=$(RISING_PACKAGE_TYPE) \
+    ro.rising.releasetype=$(RISING_BUILDTYPE) \
+    ro.rising.version?=$(RISING_VERSION) \
+    ro.rising.build.version=$(RISING_BUILD_VERSION) \
+    ro.rising.display.version?=$(RISING_DISPLAY_VERSION) \
+    ro.rising.platform_release_codename=$(RISING_FLAVOR) \
+    ro.rising.device=$(CURRENT_DEVICE) \
+    ro.rising.storage?=$(RISING_STORAGE) \
+    ro.rising.ram?=$(RISING_RAM) \
+    ro.rising.battery?=$(RISING_BATTERY) \
+    ro.rising.display_resolution?=$(RISING_DISPLAY)
+
+# Signing
+ifneq (eng,$(TARGET_BUILD_VARIANT))
+ifneq (,$(wildcard vendor/rising/signing/keys/releasekey.pk8))
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/rising/signing/keys/releasekey
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
+endif
+endif
